80f6bca1e1d06955fad927b34a5ab5ac6151930f
lguerin <laurent@dreamcat.fr>
2014-04-22 22:55:16 +0200

    Conf for debug mode with IntelliJ

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,17 +8,8 @@ grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: false,
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+        test: false,
+        run: false
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
@@ -40,10 +31,8 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.spring.io/milestone/"
     }
 
     dependencies {
@@ -76,5 +65,8 @@ grails.project.dependency.resolution = {
         compile ":less-asset-pipeline:1.7.0"
         //compile ":handlebars-asset-pipeline:1.3.0.1"
         runtime ':twitter-bootstrap:3.1.1'
+
+        // Spring plugins
+        compile ":spring-security-core:2.0-RC2"
     }
 }
