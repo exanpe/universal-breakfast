06d037152bf50455fe418f409a05303c00720cc9
jmaupoux <jmaupoux@gmail.com>
2014-06-20 19:58:19 +0200

    #34 #35 RG

--- grails-app/controllers/fr/exanpe/universal/breakfast/controller/CardController.groovy
+++ grails-app/controllers/fr/exanpe/universal/breakfast/controller/CardController.groovy
@@ -6,6 +6,7 @@ import fr.exanpe.universal.breakfast.domain.Team
 class CardController {
 
     def ubService
+    def springSecurityService
 
     def member() {
         if(!params.teamName || !params.memberName){
@@ -13,7 +14,7 @@ class CardController {
         }
         def team = Team.findByUsernameCI(params.teamName).get()
 
-        if(!team){
+        if(!allowDisplay(team)){
             return redirect(controller: "home")
         }
 
@@ -33,12 +34,19 @@ class CardController {
 
         def team = Team.findByUsernameCI(params.teamName).get()
 
-        if(!team){
+        if(!allowDisplay(team)){
             return redirect(controller: "home")
         }
 
         def count = Member.countActives(team).get()
 
         [team : team, memberCount : count]
+    }
+
+    private boolean allowDisplay(Team team){
+        //if team and team allow or team is connected
+        return team &&
+                (team.configuration.cardEnabled == true ||
+                        (springSecurityService.currentUser && springSecurityService.currentUser.id == team.id))
     }
 }
